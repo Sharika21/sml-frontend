fdf20532a03094d7f60828cc9c89e196d862a5d2
Sharika Kanti Narsing <skantinarsing@gmail.com>
2024-05-14 21:13:19 +0200

    Create homepage cards & create about us page

--- smlSystem/src/app/app.module.ts
+++ smlSystem/src/app/app.module.ts
@@ -3,25 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenubarComponent } from './menubar/menubar.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenubarComponent,
     ToolbarComponent,
+    HomepageComponent,
+    AboutUsPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatMenuModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [
     provideAnimationsAsync()
