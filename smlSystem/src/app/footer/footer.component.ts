6f40572522216122242340c0ce31199c6fd81939
Sharika Kanti Narsing <skantinarsing@gmail.com>
2024-05-18 18:07:26 +0200

    Fix styling

--- smlSystem/src/app/footer/footer.component.ts
+++ smlSystem/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  constructor(private router: Router) {}
 
+  navigateToAboutSml() {
+    this.router.navigate(['/contact-us']);
+  }
 }
